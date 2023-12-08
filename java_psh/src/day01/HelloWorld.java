90fb933b525e0c456e2c95dcdca333a98d8dde72
20231208kh <mimsso@naver.com>
2023-12-08 14:24:22 +0900

    자바 1일차 샘플 코드 및 설명

---
+++ java_psh/src/day01/HelloWorld.java
@@ -0,0 +1,26 @@
+package day01;
+
+
+// 한 줄 주석
+
+/*
+ * 여러줄 주석
+ */
+
+/**
+ *   메소드를 설명할 때 사용하는 주석
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//sysout + ctrl + space = 자동완성
+		
+		System.out.println("Hello world!");
+	    
+	    
+	}
+
+}
